f9c7af3dad5887a0f0b5f27f41c5c95c574f4120
Rouzrise <rouzrise@gmail.com>
2018-07-01 23:24:57 +0300

    Add mapbox functionality

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1Ijoicm91enJpc2UiLCJhIjoiY2pqM2EwY25vMTg1aTNrbHB0NG91NTN4cSJ9.Uc3b5e2hrp1PH7yMgzZFdQ',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
